f18fdbac7a314e1eb1b895d05c14658f851e5fdd
Raphael Pierzina <raphael@hackebrot.de>
2017-09-09 11:57:30 +0200

    Move filters to new file and implement public API

--- turtle.go
+++ turtle.go
@@ -1,7 +1,5 @@
 package turtle
 
-import "strings"
-
 // Emojis maps a name to an Emoji
 var Emojis = make(map[string]*Emoji)
 
@@ -11,39 +9,17 @@ func init() {
 	}
 }
 
-// filter a given slice of Emoji by f
-func filter(emojis []*Emoji, f func(e *Emoji) bool) []*Emoji {
-	var r []*Emoji
-	for _, e := range emojis {
-		if f(e) {
-			r = append(r, e)
-		}
-	}
-	return r
+// Search emojis by a name
+func Search(s string) []*Emoji {
+	return search(emojis, s)
 }
 
-// category filters a slice of Emoji by Category
-func category(emojis []*Emoji, c string) []*Emoji {
-	return filter(emojis, func(e *Emoji) bool {
-		return e.Category == c
-	})
+// Keyword filters the emojis by a keyword
+func Keyword(k string) []*Emoji {
+	return keyword(emojis, k)
 }
 
-// keyword filters a slice of Emoji by Keywords
-func keyword(emojis []*Emoji, k string) []*Emoji {
-	return filter(emojis, func(e *Emoji) bool {
-		for _, keyword := range e.Keywords {
-			if keyword == k {
-				return true
-			}
-		}
-		return false
-	})
-}
-
-// search Emoji in a slice by Name
-func search(emojis []*Emoji, s string) []*Emoji {
-	return filter(emojis, func(e *Emoji) bool {
-		return strings.Contains(e.Name, s)
-	})
+// Category filters the emojis by a category
+func Category(c string) []*Emoji {
+	return category(emojis, c)
 }

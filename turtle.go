d14503cc34a819dc071c4c7f38b6a6cec365128e
Raphael Pierzina <raphael@hackebrot.de>
2017-09-09 11:54:59 +0200

    Add search, keyword, category filter functions

--- turtle.go
+++ turtle.go
@@ -1,5 +1,7 @@
 package turtle
 
+import "strings"
+
 // Emojis maps a name to an Emoji
 var Emojis = make(map[string]*Emoji)
 
@@ -18,4 +20,30 @@ func filter(emojis []*Emoji, f func(e *Emoji) bool) []*Emoji {
 		}
 	}
 	return r
+}
+
+// category filters a slice of Emoji by Category
+func category(emojis []*Emoji, c string) []*Emoji {
+	return filter(emojis, func(e *Emoji) bool {
+		return e.Category == c
+	})
+}
+
+// keyword filters a slice of Emoji by Keywords
+func keyword(emojis []*Emoji, k string) []*Emoji {
+	return filter(emojis, func(e *Emoji) bool {
+		for _, keyword := range e.Keywords {
+			if keyword == k {
+				return true
+			}
+		}
+		return false
+	})
+}
+
+// search Emoji in a slice by Name
+func search(emojis []*Emoji, s string) []*Emoji {
+	return filter(emojis, func(e *Emoji) bool {
+		return strings.Contains(e.Name, s)
+	})
 }
--- turtle_test.go
+++ turtle_test.go
@@ -7,41 +7,41 @@ import (
 	"github.com/hackebrot/go-repr/repr"
 )
 
-func Test_filter(t *testing.T) {
-	emojis := []*Emoji{
-		{
-			Name:     "turtle",
-			Category: "animals_and_nature",
-			Char:     "🐢",
-			Keywords: []string{"animal", "slow", "nature", "tortoise"},
-		},
-		{
-			Name:     "coffee",
-			Category: "food_and_drink",
-			Char:     "☕",
-			Keywords: []string{"beverage", "caffeine", "latte", "espresso"},
-		},
-		{
-			Name:     "woman_technologist",
-			Category: "people",
-			Char:     "👩‍💻",
-			Keywords: []string{"coder", "developer", "engineer", "programmer", "software", "woman", "human"},
-		},
-		{
-			Name:     "dog",
-			Category: "animals_and_nature",
-			Char:     "🐶",
-			Keywords: []string{"animal", "friend", "nature", "woof", "puppy", "pet", "faithful"},
-		},
-	}
+var testEmojis = []*Emoji{
+	{
+		Name:     "turtle",
+		Category: "animals_and_nature",
+		Char:     "🐢",
+		Keywords: []string{"animal", "slow", "nature", "tortoise"},
+	},
+	{
+		Name:     "coffee",
+		Category: "food_and_drink",
+		Char:     "☕",
+		Keywords: []string{"beverage", "caffeine", "latte", "espresso"},
+	},
+	{
+		Name:     "woman_technologist",
+		Category: "people",
+		Char:     "👩‍💻",
+		Keywords: []string{"coder", "developer", "engineer", "programmer", "software", "woman", "human"},
+	},
+	{
+		Name:     "dog",
+		Category: "animals_and_nature",
+		Char:     "🐶",
+		Keywords: []string{"animal", "friend", "nature", "woof", "puppy", "pet", "faithful"},
+	},
+}
 
+func Test_filter(t *testing.T) {
 	tests := []struct {
 		name string
 		f    func(e *Emoji) bool
 		want []*Emoji
 	}{
 		{
-			name: "no match",
+			name: "no matches",
 			f: func(e *Emoji) bool {
 				return e.Name == "nope"
 			},
@@ -90,8 +90,126 @@ func Test_filter(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := filter(emojis, tt.f); !cmp.Equal(got, tt.want) {
+			if got := filter(testEmojis, tt.f); !cmp.Equal(got, tt.want) {
 				t.Errorf("filter() = %v, want %v", repr.Repr(got), repr.Repr(tt.want))
+			}
+		})
+	}
+}
+
+func Test_category(t *testing.T) {
+	tests := []struct {
+		name string
+		c    string
+		want []*Emoji
+	}{
+		{
+			name: "no matches",
+			c:    "activity",
+			want: nil,
+		},
+		{
+			name: "multiple matches",
+			c:    "animals_and_nature",
+			want: []*Emoji{
+				{
+					Name:     "turtle",
+					Category: "animals_and_nature",
+					Char:     "🐢",
+					Keywords: []string{"animal", "slow", "nature", "tortoise"},
+				},
+				{
+					Name:     "dog",
+					Category: "animals_and_nature",
+					Char:     "🐶",
+					Keywords: []string{"animal", "friend", "nature", "woof", "puppy", "pet", "faithful"},
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := category(testEmojis, tt.c); !cmp.Equal(got, tt.want) {
+				t.Errorf("category() = %v, want %v", repr.Repr(got), repr.Repr(tt.want))
+			}
+		})
+	}
+}
+
+func Test_keyword(t *testing.T) {
+	tests := []struct {
+		name string
+		k    string
+		want []*Emoji
+	}{
+		{
+			name: "no matches",
+			k:    "weather",
+			want: nil,
+		},
+		{
+			name: "single match",
+			k:    "programmer",
+			want: []*Emoji{
+				{
+					Name:     "woman_technologist",
+					Category: "people",
+					Char:     "👩‍💻",
+					Keywords: []string{"coder", "developer", "engineer", "programmer", "software", "woman", "human"},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := keyword(testEmojis, tt.k); !cmp.Equal(got, tt.want) {
+				t.Errorf("keyword() = %v, want %v", repr.Repr(got), repr.Repr(tt.want))
+			}
+		})
+	}
+}
+
+func Test_search(t *testing.T) {
+	tests := []struct {
+		name string
+		s    string
+		want []*Emoji
+	}{
+		{
+			name: "no matches",
+			s:    "nope",
+			want: nil,
+		},
+		{
+			name: "substring",
+			s:    "technologist",
+			want: []*Emoji{
+				{
+					Name:     "woman_technologist",
+					Category: "people",
+					Char:     "👩‍💻",
+					Keywords: []string{"coder", "developer", "engineer", "programmer", "software", "woman", "human"},
+				},
+			},
+		},
+		{
+			name: "full string",
+			s:    "woman_technologist",
+			want: []*Emoji{
+				{
+					Name:     "woman_technologist",
+					Category: "people",
+					Char:     "👩‍💻",
+					Keywords: []string{"coder", "developer", "engineer", "programmer", "software", "woman", "human"},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := search(testEmojis, tt.s); !cmp.Equal(got, tt.want) {
+				t.Errorf("search() = %v, want %v", repr.Repr(got), repr.Repr(tt.want))
 			}
 		})
 	}

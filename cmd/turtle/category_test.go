fb99e7483eeb742883178ec7ef6ea3a235671d38
Raphael Pierzina <raphael@hackebrot.de>
2020-05-29 10:21:26 +0200

    Refactor tests for category cmd

--- cmd/turtle/category_test.go
+++ cmd/turtle/category_test.go
@@ -1,32 +1,17 @@
 package main
 
-import (
-	"testing"
-)
+import "testing"
 
 func TestRunCategory(t *testing.T) {
-	tests := []struct {
-		name    string
-		args    []string
-		wantErr bool
-	}{
-		{
-			name:    "error",
-			args:    []string{"nope"},
-			wantErr: true,
-		},
-		{
-			name:    "no error",
-			args:    []string{"people"},
-			wantErr: false,
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if err := runCategory(nil, tt.args); (err != nil) != tt.wantErr {
-				t.Errorf("runCategory() error = %v, wantErr %v", err, tt.wantErr)
-			}
-		})
-	}
+	tests := []cmdTestCase{{
+		name:      "error",
+		args:      []string{"category"},
+		wantError: true,
+	}, {
+		name:      "animals_and_nature",
+		args:      []string{"category", "animals_and_nature"},
+		outFile:   "category/animals_and_nature.json",
+		wantError: false,
+	}}
+	runTestCmd(t, tests)
 }

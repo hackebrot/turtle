499af57d5377c8eb31096424f900fbb309c6d2a1
Raphael Pierzina <raphael@hackebrot.de>
2020-05-29 08:52:24 +0200

    Refactor test for random cmd

--- cmd/turtle/random_test.go
+++ cmd/turtle/random_test.go
@@ -1,26 +1,12 @@
 package main
 
-import (
-	"testing"
-)
+import "testing"
 
 func TestRunRandom(t *testing.T) {
-	tests := []struct {
-		name    string
-		args    []string
-		wantErr bool
-	}{
-		{
-			name:    "success",
-			args:    nil,
-			wantErr: false,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if err := runRandom(nil, tt.args); (err != nil) != tt.wantErr {
-				t.Errorf("runRandom() error = %v, wantErr %v", err, tt.wantErr)
-			}
-		})
-	}
+	tests := []cmdTestCase{{
+		name:      "random",
+		args:      []string{"random"},
+		wantError: false,
+	}}
+	runTestCmd(t, tests)
 }
